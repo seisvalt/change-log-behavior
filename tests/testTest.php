2ace876ec7a434981f71ebf6dfcc3c017333de7f
seisvalt <jl.vargass@gmail.com>
2019-02-12 08:16:46 -0500

    eliminada etiqueta cranky

--- tests/testTest.php
+++ tests/testTest.php
@@ -1,11 +1,5 @@
 <?php
 
-    /**
-     * Created by PhpStorm.
-     * User: Cranky4
-     * Date: 15.07.2016
-     * Time: 15:56
-     */
     class testTest extends PHPUnit_Framework_TestCase
     {
         public function testStart()
